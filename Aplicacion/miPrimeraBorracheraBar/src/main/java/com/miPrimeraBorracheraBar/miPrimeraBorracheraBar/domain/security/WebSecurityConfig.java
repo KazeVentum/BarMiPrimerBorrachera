9de24df2ab636104877596498af2158dc5896427
KazeVentum <kazeventum@gmail.com>
2024-10-10 10:54:35 -0500

    fix: :bug: Fixed problem about CORS config where canceled queries POST

--- Aplicacion/miPrimeraBorracheraBar/src/main/java/com/miPrimeraBorracheraBar/miPrimeraBorracheraBar/domain/security/WebSecurityConfig.java
+++ Aplicacion/miPrimeraBorracheraBar/src/main/java/com/miPrimeraBorracheraBar/miPrimeraBorracheraBar/domain/security/WebSecurityConfig.java
@@ -18,29 +18,20 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-
         http
+                .cors().and() // Agrega CORS
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authz -> authz
                         .requestMatchers(HttpMethod.POST, Constans.LOGIN_URL, Constans.REGISTER_URL).permitAll()
-                        // Allow Swagger documentation HTML
                         .requestMatchers(HttpMethod.GET,
-                                "/doc/**",
-                                "/swagger-ui.html",
-                                "/swagger-ui/**",
-                                "/v3/api-docs/**",
-                                "/api/v1/auth/**",
-                                "/v2/api-docs",
-                                "/v3/api-docs/**",
-                                "/swagger-resources",
-                                "/swagger-resources/**",
-                                "/configuration/ui",
-                                "/configuration/security",
-                                "/swagger-ui/**",
-                                "/webjars/**",
-                                "/swagger-ui.html"
+                                "/doc/**", "/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**",
+                                "/api/v1/auth/**", "/v2/api-docs", "/swagger-resources",
+                                "/swagger-resources/**", "/configuration/ui",
+                                "/configuration/security", "/swagger-ui/**",
+                                "/webjars/**", "/swagger-ui.html"
                         ).permitAll()
-                        .anyRequest().authenticated())
+                        .anyRequest().authenticated()
+                )
                 .addFilterAfter(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();

f57e5c7200e4f3bd8f4d9a44b8c2b9e4500d17e9
KazeVentum <kazeventum@gmail.com>
2024-10-28 15:31:39 -0500

    feat: :sparkles: add functionality between Create and view sites buttons and site.js file

--- Aplicacion/miPrimeraBorracheraBar/page/functionality/app.js
+++ Aplicacion/miPrimeraBorracheraBar/page/functionality/app.js
@@ -1,10 +1,22 @@
 import * as empleado from "./empleado.js";
+import * as sede from "./sede.js";
 
+//Empleado
 document.getElementById('createEmployee_btn').addEventListener("click", function() {
     empleado.crearEmpleados()
 })
 
 document.getElementById('showEmployee_btn').addEventListener("click", function() {
     empleado.MostrarEmpleados()
+})
+
+
+//Sede
+document.getElementById('createSede_btn').addEventListener("click", function() {
+    sede.CrearSedes()
+})
+
+document.getElementById('showSede_btn').addEventListener("click", function() {
+    sede.MostrarSedes()
 })
 
